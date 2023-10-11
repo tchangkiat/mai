97945ccdbfe9ed121f3ff9183c4583ad5bf686cd
Tan Chang Kiat <tanchangkiat@gmail.com>
2023-10-11 14:19:43 +0800

    Refactored code

--- mai.py
+++ mai.py
@@ -9,17 +9,6 @@ from langchain.memory import ConversationBufferMemory
 
 from utils import bedrock
 
-def send(input_text, output_text, conversation):
-    input = input_text.get("1.0", "end-1c")
-    if input:
-        ai_response = conversation.predict(input=input)
-        output_text.config(state=tk.NORMAL)
-        output_text.insert(tk.END, "[You] " + input + "\n\n")
-        output_text.insert(tk.END, "[AI] " + ai_response + "\n\n")
-        output_text.config(state=tk.DISABLED)
-        input_text.delete('1.0', tk.END)
-        output_text.see("end")
-
 class Gui(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
@@ -35,25 +24,36 @@ class Gui(tk.Frame):
         cl_llm = Bedrock(
             model_id="anthropic.claude-v2",
             client=boto3_bedrock,
-            model_kwargs={"max_tokens_to_sample": 1000},
+            model_kwargs={"max_tokens_to_sample": 300},
         )
         memory = ConversationBufferMemory()
-        conversation = ConversationChain(
+        self.conversation = ConversationChain(
             llm=cl_llm, verbose=True, memory=memory
         )
 
         # Output Text
-        output_text = tkst.ScrolledText(self, height = 30, width = 125)
-        output_text.config(state=tk.DISABLED)
+        self.output_text = tkst.ScrolledText(self, height = 30, width = 125)
+        self.output_text.config(state=tk.DISABLED)
+        self.output_text.grid(row=0, column=0, columnspan=5)
 
         # Input Text
-        output_text.grid(row=0, column=0, columnspan=5)
-        input_text = tkst.ScrolledText(self, height = 15, width = 100)
-        input_text.grid(row=1, column=0)
+        self.input_text = tkst.ScrolledText(self, height = 15, width = 100)
+        self.input_text.grid(row=1, column=0)
 
         # Send Button
-        button = tk.Button(self, text='Send', width=10, height=3, command=lambda:send(input_text, output_text, conversation)) 
+        button = tk.Button(self, text='Send', width=10, height=3, command=lambda:self.send()) 
         button.grid(row=1, column=4)
+
+    def send(self):
+        input = self.input_text.get("1.0", tk.END)
+        if input:
+            ai_response = self.conversation.predict(input=input)
+            self.output_text.config(state=tk.NORMAL)
+            self.output_text.insert(tk.END, "[You] " + input + "\n\n")
+            self.output_text.insert(tk.END, "[AI] " + ai_response + "\n\n")
+            self.output_text.config(state=tk.DISABLED)
+            self.input_text.delete('1.0', tk.END)
+            self.output_text.see("end")
 
 if __name__ == '__main__':
     # Clear the console screen
