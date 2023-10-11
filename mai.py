aef948f53773286599755738b32d6b40997f1c72
Tan Chang Kiat <tanchangkiat@gmail.com>
2023-10-11 23:44:41 +0800

    Added Retrieval Augmented Generation (RAG) using information on AWS Enterprise Support

--- mai.py
+++ mai.py
@@ -5,12 +5,25 @@ from langchain.chains import ConversationChain
 from langchain.llms.bedrock import Bedrock
 from langchain.memory import ConversationBufferMemory
 
+# Dependencies for embedding text in a vector store
+from langchain.embeddings import BedrockEmbeddings
+from langchain.document_loaders import CSVLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain.vectorstores import FAISS
+from langchain.chains.conversational_retrieval.prompts import CONDENSE_QUESTION_PROMPT
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+
 from utils import bedrock, polly
 
 from pygame import mixer
 
 class Mai():
     def __init__(self):
+        self.rag = False # Change to True to use the provided context stored in FAISS
+        self.text_to_speech = False # Change to True to use Amazon Polly to synthesize the LLM's response
+
         # Used for setting up clients for Amazon services
         os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
         os.environ["AWS_PROFILE"] = "default"
@@ -45,32 +58,67 @@ class Mai():
 
         Assistant:
         """)
-
         self.conversation.prompt = claude_prompt
 
-        # Set up client for Amazon Polly
-        self.polly_client = polly.get_polly_client(
-            region=os.environ.get("AWS_DEFAULT_REGION", None)
-        )
+        if self.rag:
+            # Store text embeddings in vector store
+            br_embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1", client=boto3_bedrock)
 
-        # Initialize dependency to play sound
-        mixer.init()
+            loader = CSVLoader("./rag_data/aws-enterprise-support.csv")
+            documents_aws_es = loader.load()
+            print(f"Number of documents={len(documents_aws_es)}")
+
+            docs = CharacterTextSplitter(chunk_size=2000, chunk_overlap=400, separator=",").split_documents(documents_aws_es)
+
+            print(f"Number of documents after split and chunking={len(docs)}")
+
+            vectorstore_faiss_aws = FAISS.from_documents(
+                documents=docs,
+                embedding = br_embeddings
+            )
+
+            print(f"vectorstore_faiss_aws: number of elements in the index={vectorstore_faiss_aws.index.ntotal}::")
+
+            memory_chain = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+            self.retrieval_chain = ConversationalRetrievalChain.from_llm(
+                llm=cl_llm, 
+                retriever=vectorstore_faiss_aws.as_retriever(), 
+                memory=memory_chain,
+                condense_question_prompt=CONDENSE_QUESTION_PROMPT,
+                #verbose=True,
+                chain_type='stuff', # 'refine',
+                #max_tokens_limit=300
+            )
+
+        if self.text_to_speech:
+            # Set up client for Amazon Polly
+            self.polly_client = polly.get_polly_client(
+                region=os.environ.get("AWS_DEFAULT_REGION", None)
+            )
+
+            # Initialize dependency to play sound
+            mixer.init()
 
     def prompt(self, input):
-        return "[Mai] " + self.conversation.predict(input=input)
+        if self.rag:
+            response = "[Mai] " + self.retrieval_chain.run({'question': input })
+        else:
+            response = "[Mai] " + self.conversation.predict(input=input)
 
-    def synthesize(self, text):
-        # Use Amazon Polly to synthesize speech
-        response = self.polly_client.synthesize_speech(VoiceId='Joanna',
-            OutputFormat='mp3', 
-            Text = text,
-            Engine = 'neural')
+        if self.text_to_speech:
+            # Use Amazon Polly to synthesize speech
+            polly_response = self.polly_client.synthesize_speech(VoiceId='Joanna',
+                OutputFormat='mp3', 
+                Text = response,
+                Engine = 'neural')
 
-        # Write to an MP3 file
-        file = open('response.mp3', 'wb')
-        file.write(response['AudioStream'].read())
-        file.close()
+            # Write to an MP3 file
+            file = open('response.mp3', 'wb')
+            file.write(polly_response['AudioStream'].read())
+            file.close()
 
-        # Play MP3
-        mixer.music.load("response.mp3")
-        mixer.music.play()
+            # Play MP3
+            mixer.music.load("response.mp3")
+            mixer.music.play()
+
+        return response
